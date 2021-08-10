3bb75797421bb7c6148cc9c25227a7e32a414249
Thu Dang <thudang.dtt@gmail.com>
2021-08-09 20:08:00 -0500

    first commit

---
+++ collapsible.js
@@ -0,0 +1,13 @@
+let expand = document.getElementsByClassName("question");
+
+for (let i = 0; i < expand.length; i++) {
+  expand[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
